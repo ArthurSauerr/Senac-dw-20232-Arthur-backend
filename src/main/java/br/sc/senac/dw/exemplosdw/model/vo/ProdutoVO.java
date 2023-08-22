a2a9155c22123206fde25b5f27eb5370d2e13f9d
Arthur Sauer <germanooarthuur@gmail.com>
2023-08-22 08:29:21 -0300

    Ajustes no VO e properties

--- src/main/java/br/sc/senac/dw/exemplosdw/model/vo/ProdutoVO.java
+++ src/main/java/br/sc/senac/dw/exemplosdw/model/vo/ProdutoVO.java
@@ -1,24 +1,35 @@
 package br.sc.senac.dw.exemplosdw.model.vo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
 @Getter
 @Setter
+@Entity
+@Table(name = "produtos")
 public class ProdutoVO {
+
+    @Id
+    private Integer id;
     private String nome;
     private String fabricante;
-    private double preco;
-    private int quantidade;
-    private LocalDate data;
+    private double valor;
+    private double peso;
 
-    public ProdutoVO(String nome, String fabricante, double preco, int quantidade, LocalDate data) {
+    private LocalDate data_cadastro;
+
+
+    public ProdutoVO(String nome, String fabricante, double valor, double peso, LocalDate data_cadastro) {
         this.nome = nome;
         this.fabricante = fabricante;
-        this.preco = preco;
-        this.quantidade = quantidade;
-        this.data = data;
+        this.valor = valor;
+        this.peso = peso;
+        this.data_cadastro = data_cadastro;
     }
 
     public ProdutoVO() {
