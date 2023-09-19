3bdb96e9f8643aae4833125eab717c494b83d194
Arthur Sauer <germanooarthuur@gmail.com>
2023-09-19 11:01:11 -0300

    Banco atualizado

--- src/main/resources/scripts/teste.sql
+++ src/main/resources/scripts/teste.sql
@@ -1,16 +1,19 @@
-drop schema EXEMPLOS;
+drop schema teste;
 
-CREATE SCHEMA EXEMPLOS;
-USE EXEMPLOS;
+CREATE SCHEMA teste;
+USE teste;
 
-CREATE TABLE exemplos.fabricantes (
+CREATE TABLE teste.fabricantes (
     id int auto_increment NOT NULL,
     nome varchar(400) NOT NULL,
     cnpj varchar(14) NOT NULL,
+    cidade varchar(100) NOT NULL,
+    cep int NOT NULL,
+    uf varchar(2) NOT NULL,
     CONSTRAINT fabricantes_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE exemplos.produtos (
+CREATE TABLE teste.produtos (
     id int auto_increment NOT NULL,
     nome varchar(400) NOT NULL,
     id_fabricante int NOT NULL,
@@ -18,29 +21,9 @@ CREATE TABLE exemplos.produtos (
     peso DECIMAL NOT NULL,
     data_cadastro DATE NOT NULL,
     CONSTRAINT produtos_pk PRIMARY KEY (id),
-    CONSTRAINT id_fabricante foreign key (id_fabricante) references exemplos.fabricantes(ID)
+    CONSTRAINT id_fabricante foreign key (id_fabricante) references teste.fabricantes(ID)
 );
 
-INSERT INTO exemplos.fabricantes (nome, cnpj)
-VALUES('Ouro', '11222333000022');
 
-INSERT INTO exemplos.fabricantes (nome, cnpj)
-VALUES('Pelé', '10222333000022');
-
-INSERT INTO exemplos.fabricantes (nome, cnpj)
-VALUES('Havaianas', '55222333000022');
-
-INSERT INTO exemplos.fabricantes (nome, cnpj)
-VALUES('Rider', '88222333000022');
-
-INSERT INTO exemplos.produtos (nome, id_fabricante, valor, peso, data_cadastro)
-VALUES('Café', 1, 12.5, 0.5, sysdate());
-
-INSERT INTO exemplos.produtos (nome, id_fabricante, valor, peso, data_cadastro)
-VALUES('Café', 2, 10.0, 0.5, sysdate());
-
-INSERT INTO exemplos.produtos (nome, id_fabricante, valor, peso, data_cadastro)
-VALUES('Chinela de tira', 3, 80.5, 0.2, sysdate());
-
-INSERT INTO exemplos.produtos (nome, id_fabricante, valor, peso, data_cadastro)
-VALUES('Chinela pesada', 4, 70.2, 0.3, sysdate());
+select * from teste.produtos;
+select * from teste.fabricantes;
